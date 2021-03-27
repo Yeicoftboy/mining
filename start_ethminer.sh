bc3171841e29224841867ba807774b3e357ea70c
Yeicoftboy <73478916+Yeicoftboy@users.noreply.github.com>
2021-03-26 20:37:42 -0400

    Update start_ethminer.sh

--- start_ethminer.sh
+++ start_ethminer.sh
@@ -1,4 +1,4 @@
-#
+# remplace La billetera y ponga la de usted, Seguido con punto y el nombre de usuario de mineria, luego @ y ponga el grupo de mineria que desea.
 # Example shell file for starting ethminer
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./ethminer/ethminer -P stratum1+tcp://0xc4a02f2683d45dad7efb5bd29909b9e4d27eddf7.Rig001@eth-eu1.nanopool.org:9999
+./ethminer/ethminer -P stratum1+tcp://0x8349ACdBd66b35131Da49672E1010793866b85f4.Fucksociety@eth.2miners.com:2020
