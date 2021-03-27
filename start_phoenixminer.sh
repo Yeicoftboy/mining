cd72ce2be919c9c29eaeb2517e669e9f0dc82003
Yeicoftboy <73478916+Yeicoftboy@users.noreply.github.com>
2021-03-27 02:21:18 -0400

    Update start_phoenixminer.sh

--- start_phoenixminer.sh
+++ start_phoenixminer.sh
@@ -2,4 +2,4 @@
 # Example shell file for starting PhoenixMiner to mine ETH
 #
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./phoenixminer/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x8349ACdBd66b35131Da49672E1010793866b85f4/Fucksociety -pass x
+./phoenixminer/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x70634360df80f1c02060079c301e26330da2dee9/Fucksociety -pass x
